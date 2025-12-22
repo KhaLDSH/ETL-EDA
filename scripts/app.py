3253c7d946dd88691ba64bc21a3e325a7cb3a0d9
khaldsh <kh.learn4@gmail.com>
2025-12-22 20:36:39 +0300

    update

--- scripts/app.py
+++ scripts/app.py
@@ -1,12 +1,14 @@
-from src.bootcamp_data.io import read_orders_csv
-from src.bootcamp_data.io import write_parquet
+from src.bootcamp_data.config import make_paths
+# from bootcamp_data.io import write_parquet
 from pathlib import Path
+import sys
 
 import pandas as pd
 ROOT = Path(__file__).resolve().parents[1]
 
-path = Path("data//raw/orders.csv")
-df = read_orders_csv(path)
-print(df)
-print(ROOT)
-# print(Path.chmod())
+# path = Path("data//raw/orders.csv")
+# df = read_orders_csv(path)
+# print(df)
+print("\n",ROOT, "")
+print(sys.executable)
+# print("\n\n",make_paths(ROOT), "\n\n")
