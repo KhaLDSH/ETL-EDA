277c74ee2f9d538d18150781e0e707cc562c46c7
khaldsh <kh.learn4@gmail.com>
2025-12-22 20:37:07 +0300

    update

---
+++ scripts/run_day1_load.py
@@ -0,0 +1,14 @@
+# from bootcamp_data.config import make_paths
+# from bootcamp_data.io import write_parquet
+from pathlib import Path
+import sys
+
+import pandas as pd
+ROOT = Path(__file__).resolve().parents[1]
+
+# path = Path("data//raw/orders.csv")
+# df = read_orders_csv(path)
+# print(df)
+print("\n",ROOT, "")
+print(sys.executable)
+# print("\n\n",make_paths(ROOT), "\n\n")
