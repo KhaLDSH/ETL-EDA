3488c029b806978e744557fe404b4cd614896d9d
khaldsh <kh.learn4@gmail.com>
2025-12-22 20:39:40 +0300

    update

--- src/bootcamp_data/transforms.py
+++ src/bootcamp_data/transforms.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def enforce_schema(df: pd.DataFrame) -> pd.DataFrame:
     return df.assign(
-        order_id= df["oreder_id"].astype("string"),
-        user_id= df["user_id"].astype("string"),
-        amount= pd.to_numeric(df["amount"], errors="coerce").astype("Float64"),
-        quantity= pd.to_numeric(df["quantity"], errors="coerce").astype("Float64")
+        order_id=df["order_id"].astype("string"),
+        user_id=df["user_id"].astype("string"),
+        amount=pd.to_numeric(df["amount"], errors="coerce").astype("Float64"),
+        quantity=pd.to_numeric(df["quantity"], errors="coerce").astype("Int64"),
     )
